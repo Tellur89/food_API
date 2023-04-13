7adf5368bf17c20bdd93f2736efd8ae7125b3c6a
Tom Burzynski <tellur.beryl@gmail.com>
2023-04-13 17:59:39 +0100

    finish API but needs styling

--- app.js
+++ app.js
@@ -12,14 +12,75 @@ app.use(cors());
 app.use(express.json());
 // -----
 
+// GET
 app.get('/', (req, res) => {
 	res.sendFile(index);
 });
 
 app.get('/meals', (req, res) => {
-	res.send(fruits);
+	res.send(meals);
+});
+
+app.get('/meals/:name', (req, res) => {
+	const name = req.params.name.toLowerCase();
+	const meal = meals.find((el) => el.name.toLowerCase() === name);
+
+	meal === undefined ? res.status(404).json({ error: `meal: ${name} not found` }) : res.send(meal);
+});
+// -----
+
+// POST
+app.post('/meals', (req, res) => {
+	const ids = meals.map((el) => el.id);
+	let maxId = Math.max(...ids);
+
+	const meal = meals.find((el) => el.name === req.body.name);
+
+	if (meal !== undefined) {
+		res.status(409).send({ error: 'Meal already exist' });
+	} else {
+		maxId += 1;
+		const newMeal = req.body;
+		newMeal.id = maxId;
+		meals.push(newMeal);
+		res.status(201).send(newMeal);
+	}
+});
+// -----
+
+// PATCH
+app.patch('/meals/:id', (req, res) => {
+	const id = req.params.id;
+	const meal = meals.find((el) => el.id === req.params.id);
+
+	if (meal === undefined) {
+		return res.status(404).send({ error: 'Meal does not exist' });
+	}
+
+	try {
+		const updatedMeal = { ...req.body, id: meal.id };
+		const idIndex = meals.findIndex((el) => el.id === meal.id);
+		meals[idIndex] = updatedMeal;
+		res.send(updatedMeal);
+	} catch (err) {
+		res.status(400).send('Could not update it.');
+	}
+});
+// -----
+
+// DELETE
+app.delete('/meals/:id', (req, res) => {
+	const meal = req.params.id;
+
+	const mealIndex = meals.findIndex((el) => el.id === meal);
+
+	if (mealIndex === -1) {
+		res.status(404).send({ error: 'Meal does not exist' });
+	} else {
+		meals.splice(mealIndex, 1);
+
+		res.status(204).send();
+	}
 });
 
 module.exports = app;
-
-// const url = 'https://raw.githubusercontent.com/Tellur89/food_json/main/food.json';
