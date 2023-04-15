8f7b0efd7aa752e23617ea28be0e84d788539e06
Tom Burzynski <tellur.beryl@gmail.com>
2023-04-15 23:43:02 +0100

    add new features for adding meals

--- app.js
+++ app.js
@@ -3,8 +3,22 @@ const cors = require('cors');
 const path = require('path');
 const meals = require('./meals.json');
 const { capitalise } = require('./helpers');
+const bodyParser = require('body-parser');
+
+class NewMeal {
+	constructor(name, id, price, sauce, img, gluten) {
+		this.name = name;
+		this.id = id;
+		this.price = price;
+		this.sauce = sauce;
+		this.img = img;
+		this.gluten = gluten;
+	}
+}
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 // const index = path.join(__dirname, './index.html');
 
@@ -14,9 +28,12 @@ app.use(express.json());
 // -----
 
 // GET
-app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', (req, res) => {
+// 	res.sendFile(__dirname + '/index.html');
+// });
+// app.get('/', (req, res) => {
+// 	res.sendFile(__dirname + '/addMeal.html');
+// });
 
 app.get('/meals', (req, res) => {
 	res.send(meals);
@@ -46,6 +63,21 @@ app.post('/meals', (req, res) => {
 		meals.push(newMeal);
 		res.status(201).send(newMeal);
 	}
+});
+
+app.post('/add', (req, res) => {
+	const ids = meals.map((el) => el.id);
+	let maxId = Math.max(...ids);
+	// maxId += 1;
+	const newMeal = new NewMeal(req.body.mealName, (maxId += 1), req.body.mealPrice, req.body.mealSauce, req.body.mealImg, req.body.mealGluten);
+	meals.push(newMeal);
+	res.status(201).send(newMeal);
+
+	// newMeal.save(function (err) {
+	// 	if (!err) {
+	// 		res.redirect('/');
+	// 	}
+	// });
 });
 // -----
 
